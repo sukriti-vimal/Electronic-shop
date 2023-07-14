f99a3447eceba81a506c4caf21a2a3a00d00c8e1
sukriti-vimal <sukriti.208406@knit.ac.in>
2023-07-14 20:11:06 +0530

    updated

--- data/database.js
+++ data/database.js
@@ -4,8 +4,8 @@ const MongoClient = mongodb.MongoClient;
 
 let mongodbUrl = 'mongodb://127.0.0.1:27017'
 
-if(process.env.MONGODB_URL){
-  mongodbUrl = process.env.MONGODB_URL;
+if(process.env.MONGODB_URI){
+  mongodbUrl = process.env.MONGODB_URI;
 }
 
 let database;
